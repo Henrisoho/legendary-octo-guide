5013f5d7bed37d5e812080e5f20f0e46421ac7c4
Bishar00 <bishar1yusuf@gmail.com>
2023-01-19 16:55:30 -0600

    input for order total

--- src/components/HomePage/OrderTotal.jsx
+++ src/components/HomePage/OrderTotal.jsx
@@ -1,12 +1,51 @@
 import React from "react";
 
 function OrderTotal() {
-
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [zip, setZip] = useState('');
 
 return (
     <>
-      order total goes here
-    </>
+     <section>
+      <h2>Step 2: Customer Information</h2>
+      <form onSubmit={handleSubmit} className="add-Order-form">
+        <input 
+          required 
+          placeholder="Name"                
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          <button type="Pickup"> Pickup </button>
+        />
+
+        <input 
+          required 
+          placeholder="Street Address" 
+          value={address}
+          onChange={(event) => setAddress(event.target.value)}
+          <button type="Delivery"> Delivery </button>
+        />
+
+        <input 
+          required 
+          placeholder="City" 
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+        />
+
+          <input 
+          required 
+          placeholder="Zip" 
+          value={city}
+          onChange={(event) => setZip(event.target.value)}
+        />
+
+        <button type="submit">
+          Next
+        </button>
+      </form>
+    </section>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import './index.css';
 import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+function RemoveOrder(arr, id) {
+    const objWithIdIndex = arr.findIndex((obj) => obj.id === id);
+    if (objWithIdIndex > -1) {
+      arr.pop(objWithIdIndex, 1);
+    }
+    return NewOrder;
+  }
+  
+  removeObjectWithId(arr, 2);
+    
