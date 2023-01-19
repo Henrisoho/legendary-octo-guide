5b50db077eae7e2c1f18b9985c09c8c926145cd5
henrisoho <bakingwithmymother@gmail.com>
2023-01-19 16:56:12 -0600

    checkout

--- src/components/HomePage/AddOrder.jsx
+++ src/components/HomePage/AddOrder.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 
 function AddOrder() {
-
+    const dispatch = useDispatch();
+    const pushPizza = () => {
+        event.preventDefault();
+        console.log('alert', pizza.id);
+        dispatch({
+            type: 'CREATE_PIZZA',
+            payload: {
+                
+            }
+        });
+    }
 
 return (
     <>
-      Add pizza goes here
+      <button onClick={pushPizza}>ADD</button>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import {logger} from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ADD ITEM//
+const addOrder = 
+(state=[], action) => {
+    order
+  if (action.type === 'CREATE_PIZZA') {
+    console.log('in the CREATE_PIZZA if block:', action);
+    const newPizzas = action.payload;
+
+    
+    const newOrder = {
+	pizzas: newPizzas,
+    }
+
+    let newOrderArray = [...state]
+
+    newOrderArray.push(newOrder)
+
+    return newOrderArray;
+  }
+  if (action.type === 'CREATE_ADDRESS') {
+    console.log('in the CREATE_ADDRESS if block:', action);
+    const newAddress = action.payload;
+
+    
+    const newOrder = {
+    id: newOrderId,
+	pizzas: newPizzas,
+	address: newAddress
+    }
+
+    let newOrderArray = [...state]
+
+    newOrderArray.push(newOrder)
+
+    return newOrderArray;
+  }
+  return state;
+}
+//END ADD ITEM//
+
+
+//STORE//
+const pizzaStore = createStore(
+  combineReducers({
+    addOrder,
+  }),
+  applyMiddleware(
+    logger
+  )
+);
+//END STORE//
+
+
+ReactDOM.render(<Provider store={pizzaStore}><App /></Provider>, document.getElementById('root'));
